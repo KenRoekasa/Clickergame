5917d4ee39f17dbea919711900038351ccd09555
Kenny Roekasa <kennykr@hotmail.co.uk>
2018-10-19 00:56:26 +0100

    Add Contrustor and increase methods

--- src/GameController.java
+++ src/GameController.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class GameController {
+    private int currency;
+    private int curPerTick;
+    private int  tickPerSec;
+
+    public GameController(PlayerData data){
+        this.currency = data.currency;
+        this.curPerTick = data.curPerTick;
+        this.tickPerSec = data.tickPerSec;
+    }
+
+    private void increaseCurPerTick(int amount){
+        curPerTick += amount;
+    }
+
+    private void increaseTickPerSec(int amount){
+        tickPerSec += amount;
+    }
+
 }
