8eb9b598ef913d30cd123203497bbf85c37fd268
Kenny Roekasa <kennykr@hotmail.co.uk>
2018-10-19 01:08:52 +0100

    Update MCV model

--- src/MainController.java
+++ src/MainController.java
@@ -2,7 +2,7 @@ public class MainController {
     public static void main(String[] args){
         GuiController gui = new GuiController();
         PlayerData data = new PlayerData();
-        GameController gameController = new GameController(data);
+        GameController gameController = new GameController(data, gui);
     }
 
 }
