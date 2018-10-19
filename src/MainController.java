85f90041a05c9ae9bb4edc17d51e520de4052763
Kenny Roekasa <kennykr@hotmail.co.uk>
2018-10-19 01:01:05 +0100

    Attempted MCV model

--- src/MainController.java
+++ src/MainController.java
@@ -1,6 +1,8 @@
 public class MainController {
     public static void main(String[] args){
-
+        GuiController gui = new GuiController();
+        PlayerData data = new PlayerData();
+        GameController gameController = new GameController(data);
     }
 
 }
