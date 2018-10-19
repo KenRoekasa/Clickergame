5cae2c3070c8c1fa5eab696285f5f984d42b29d7
Kenny Roekasa <kennykr@hotmail.co.uk>
2018-10-19 01:07:14 +0100

    Add Getters and Setters

--- src/PlayerData.java
+++ src/PlayerData.java
@@ -1,5 +1,32 @@
+//Model
 public class PlayerData {
-    public int currency = 0;
-    public int curPerTick = 1;
-    public int  tickPerSec = 1;
+    private int currency = 0;
+    private int curPerTick = 1;
+    private int tickPerSec = 1;
+
+    public int getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(int currency) {
+        this.currency = currency;
+    }
+
+    public int getCurPerTick() {
+        return curPerTick;
+    }
+
+    public void setCurPerTick(int curPerTick) {
+        this.curPerTick = curPerTick;
+    }
+
+    public int getTickPerSec() {
+        return tickPerSec;
+    }
+
+    public void setTickPerSec(int tickPerSec) {
+        this.tickPerSec = tickPerSec;
+    }
+
+
 }
