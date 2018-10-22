136130723db9d6238c8fdd30e3242e0d89f5ea9b
Kenny Roekasa <kennykr@hotmail.co.uk>
2018-10-22 18:01:06 +0100

    Implemented GUI structure

--- src/GuiController.java
+++ src/GuiController.java
@@ -1,2 +1,17 @@
-public class GuiController {
+import javax.swing.*;
+
+public class GuiController extends JFrame{
+    private JFrame frame;
+    private JPanel panel;
+
+    public void GuiController(){
+        frame = new JFrame("Clicker game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
 }
