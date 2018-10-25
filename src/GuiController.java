1d3d0cbb798d0e75c8689e11af6509d99f5c1045
Kenny Roekasa <kennykr@hotmail.co.uk>
2018-10-25 17:38:43 +0100

    Add per second label and functionality

--- src/GuiController.java
+++ src/GuiController.java
@@ -5,11 +5,8 @@ import java.awt.event.ActionListener;
 
 public class GuiController {
     private JFrame frame;
-    private JPanel infoPanel;
-    private JPanel mainPanel;
-    private JPanel bottomPanel;
-    private JPanel upgradePanel;
-    private JLabel labelCurrency;
+    private JPanel infoPanel, mainPanel, bottomPanel, upgradePanel;
+    private JLabel labelCurrency, labelPerSecond;
     private JButton mainButton, exitButton, upgrade1Button, upgrade2Button, upgrade3Button, upgrade4Button;
     private GameController gameController;
 
@@ -18,29 +15,38 @@ public class GuiController {
         frame = new JFrame("Clicker game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //JPanel creation
         infoPanel = new JPanel();
         mainPanel = new JPanel();
         bottomPanel = new JPanel();
         upgradePanel = new JPanel();
 
+        //Main button Settings
         mainButton = new JButton(new ImageIcon("icons/mince.jpg"));
         mainButton.setPreferredSize(new Dimension(100, 100));
         exitButton = new JButton("EXIT");
 
+        //Upgrades Buttons
         upgrade1Button = new JButton("Oven");
         upgrade2Button = new JButton("BUY ALL");
         upgrade3Button = new JButton("Mum");
         upgrade4Button = new JButton("BUY ALL");
 
+        //Labels
         labelCurrency = new JLabel("Mince Pies: " + gameController.getPlayerData().getCurrency());
+        labelPerSecond = new JLabel("per second: " + gameController.getPlayerData().getCurPerTick());
 
-
+        BoxLayout boxLayout = new BoxLayout(infoPanel, BoxLayout.Y_AXIS) ;
+        infoPanel.setLayout(boxLayout);
+        //Adding label to info panel
         infoPanel.add(labelCurrency);
+        infoPanel.add(labelPerSecond);
 
         mainPanel.add(mainButton);
 
         bottomPanel.add(exitButton);
 
+        //Adding upgrade
         upgradePanel.add(upgrade1Button);
         upgradePanel.add(upgrade2Button);
         upgradePanel.add(upgrade3Button);
@@ -48,7 +54,7 @@ public class GuiController {
 
         upgradePanel.setLayout(new GridLayout(2, 3));
 
-
+        //Add Panel to main frame
         frame.add(infoPanel, BorderLayout.PAGE_START);
         frame.add(mainPanel, BorderLayout.CENTER);
         frame.add(exitButton, BorderLayout.PAGE_END);
@@ -57,7 +63,8 @@ public class GuiController {
         frame.pack();
         frame.setVisible(true);
 
-        GuiThread guiThread = new GuiThread(labelCurrency, gameController.getPlayerData());
+        //Thread for updating the gui
+        GuiThread guiThread = new GuiThread(labelCurrency, labelPerSecond ,gameController.getPlayerData());
         guiThread.start();
 
         mainButton.addActionListener(new ActionListener() {
@@ -87,7 +94,7 @@ public class GuiController {
         upgrade2Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	gameController.buyAllOven();
+                gameController.buyAllOven();
             }
         });
 
@@ -98,8 +105,8 @@ public class GuiController {
                 gameController.increaseCurPerTick(1);
             }
         });
-        
-      //Mum buyall
+
+        //Mum buyall
         upgrade4Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
--- src/GuiThread.java
+++ src/GuiThread.java
@@ -1,18 +1,21 @@
 import javax.swing.*;
 
 public class GuiThread extends Thread{
+    private JLabel perSecondLabel;
     private JLabel currencyLabel;
     private PlayerData data;
     private int updateGui = 10;
 
-    public GuiThread(JLabel currencyLabel, PlayerData data){
+    public GuiThread(JLabel currencyLabel, JLabel perSecondLabel, PlayerData data){
         this.currencyLabel = currencyLabel;
+        this.perSecondLabel = perSecondLabel;
         this.data = data;
     }
     @Override
     public synchronized void run() {
         while (true){
             currencyLabel.setText("Mince Pies: " + data.getCurrency());
+            perSecondLabel.setText("per second: " + data.getCurPerTick());
             try {
                 Thread.sleep(updateGui);
             } catch (InterruptedException e) {
